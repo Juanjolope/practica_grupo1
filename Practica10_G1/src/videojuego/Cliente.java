5f8749702b0479576d456690e0c5f5b214ff2f9d
aantort438 <aantort438@AANTORT-DAW>
2023-05-23 12:32:28 +0200

    Generamos los Getters y los Setters de Cliente.

--- Practica10_G1/src/videojuego/Cliente.java
+++ Practica10_G1/src/videojuego/Cliente.java
@@ -18,5 +18,29 @@ public class Cliente {
 		
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public List<Videojuego> getComprados() {
+		return comprados;
+	}
+
+	public void setComprados(List<Videojuego> comprados) {
+		this.comprados = comprados;
+	}
+
 
 }
