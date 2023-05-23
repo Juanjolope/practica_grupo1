c3b39435ea43d5a1ea964e5bf7c677fd31d9d3aa
aantort438 <aantort438@AANTORT-DAW>
2023-05-23 12:19:51 +0200

    Declaro los atributos de la clase JuegoDigital

--- Practica10_G1/src/videojuego/JuegoDigital.java
+++ Practica10_G1/src/videojuego/JuegoDigital.java
@@ -1,5 +1,11 @@
 package videojuego;
 
 public class JuegoDigital {
+	private double tamañoEnGB;
 
+    public JuegoDigital(String título, String género, String plataforma, double precio,
+                        double tamañoEnGB) {
+        super(título, género, plataforma, precio);
+        this.tamañoEnGB = tamañoEnGB;
+    }
 }
