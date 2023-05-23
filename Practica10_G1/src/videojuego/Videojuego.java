0b8d7f2807b1c1511b5719720f4187b16214ce55
Juanjo <Usuario@DESKTOP-IEP3AC2>
2023-05-23 11:21:54 +0200

    subida de codigo de la clase videojuego

--- Practica10_G1/src/videojuego/Videojuego.java
+++ Practica10_G1/src/videojuego/Videojuego.java
@@ -1,5 +1,60 @@
 package videojuego;
 
-public class Videojuego {
+public class Videojuego implements Comparable<Videojuego> {
 
+	private String titulo;
+	private String genero;
+	private String plataforma;
+	private double precio;
+	
+	public Videojuego(String titulo, String genero, String plataforma, double precio) {
+		
+		this.titulo = titulo;
+		this.genero = genero;
+		this.plataforma = plataforma;
+		this.precio = precio;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String plataforma) {
+		this.plataforma = plataforma;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	@Override
+	public int compareTo(Videojuego o) {
+	
+		return this.titulo.compareTo(o.titulo);
+	
+	}
+	
+	
+	
+	
 }
